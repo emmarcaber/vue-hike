8e93e4dc81053b03d4c1ee2c7e509d8b2b6cbb51
Emmar Caber <caberemmar@gmail.com>
2025-10-20 06:22:33 +0000

    Feat: add TodoList component for managing todos

---
+++ src/1_Core_Vue_Foundation/TodoList.vue
@@ -0,0 +1,55 @@
+<script setup>
+import { ref } from 'vue'
+
+const newTodo = ref('')
+const todos = ref(['Learn Vue.js', 'Build a Todo App', 'Master Composition API'])
+
+const addTodo = () => {
+  if (newTodo.value) {
+    todos.value.push(newTodo.value)
+    newTodo.value = ''
+  }
+}
+
+const removeTodo = (index) => {
+  todos.value.splice(index, 1)
+}
+</script>
+
+<template>
+  <div class="min-h-screen bg-gray-100 p-6">
+    <h1 class="text-4xl font-bold mb-6 text-center">Todo List</h1>
+    <div class="max-w-md mx-auto bg-white p-6 rounded-lg shadow-md">
+      <div class="flex mb-4">
+        <input
+          v-model="newTodo"
+          @keyup.enter="addTodo"
+          type="text"
+          placeholder="Add a new todo"
+          class="flex-grow px-4 py-2 border border-gray-300 rounded-l-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+        />
+        <button
+          @click="addTodo"
+          class="px-4 py-2 bg-blue-500 text-white rounded-r-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
+        >
+          Add
+        </button>
+      </div>
+      <ul>
+        <li
+          v-for="(todo, index) in todos"
+          :key="index"
+          class="flex justify-between items-center bg-gray-50 p-3 mb-2 rounded-lg"
+        >
+          <span>{{ todo }}</span>
+          <button
+            @click="removeTodo(index)"
+            class="text-red-500 hover:text-red-700 focus:outline-none"
+          >
+            Remove
+          </button>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
-import CounterApp from './1_Core_Vue_Foundation/CounterApp.vue'
+// import CounterApp from './1_Core_Vue_Foundation/CounterApp.vue'
+import TodoList from './1_Core_Vue_Foundation/TodoList.vue'
 </script>
 
 <template>
   <div id="app">
-    <CounterApp />
+    <!-- <CounterApp /> -->
+    <TodoList />
   </div>
 </template>
 
